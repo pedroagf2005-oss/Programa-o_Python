89d6af07d9850dd9cdcd9e66559b9c06d7f61c91
Pedro Augusto Gomes Fernandes <pedroagf2005@gmail.com>
2026-06-03 16:08:20 -0300

    Add Python learning material and examples

---
+++ Aulas Python/conteúdo.py
@@ -0,0 +1,153 @@
+# Todo o material que eu aprendi de python até o momento (03/06/2026)
+#tipos de dados no python
+nome = 'pedro'
+idade = 20
+altura = 1.80
+ativo = True
+#operadores
+numa = 20
+numb = 2
+soma = numa + numb
+
+subtração = numa - numb 
+
+múltiplicação = numa * numb
+
+divisão = numa / 2
+
+#comando de entrada e saída 
+nome = input ('digite nome completo: ')
+idade = input ('digite a sua idade:')
+print (f'olá, {nome}!, você tem {idade}!')
+
+#atividade do dia 2/04
+num = int(input('digite um número: '))
+if num > 0:
+    print('o número é positivo')
+elif num < 0:
+    print('o número é negativo')
+else:
+    print('o número é zero')
+idade = int(input('digite a sua idade:'))
+if idade >= 18:
+    print('você é maior de idade')
+else:
+    print('você é menor de idade')
+preço = float(input('digite o valor do produto: '))
+if preço > 100 * 0.1:
+    print(f'o preço será de {preço}')
+
+
+
+###calculadora de tabuada
+num = [1,2,3,4,5,6,7,8,9,10]
+for (multiplicador) in num:
+    print('tabuada do {multiplicador}')
+for (multiplicando) in num:
+    print(f'{multiplicador} X {multiplicando} = {multiplicador * multiplicando}')
+#fim da calculadora 
+#While
+contador = 0
+while contador < 30:
+    print(contador)
+    contador = contador + 1
+#fim do while
+#início do break
+for número in range (1,101,1):
+    if número == 50:
+        break
+    print(número)
+#fim do break
+#início do continue
+for número in range (1,101,1):
+    if número == 50:
+        continue
+    print(número)
+#fim do continue ]
+
+
+#Estrutura de Dados
+
+
+
+lista = [10, 4, -23]
+#índice:0 posição:1, valor:10
+#índice:1 posição:2, valor:4
+#índice:2 posição:3, valor:-23
+#exemplo 1:
+lista = [1,3,5,7,9]
+print(lista)
+#Exemplo 2:
+numeros = [2,7,4,9,6]
+#pecorrer a lista
+for n in numeros:
+    #analisar se é par ou ímpar
+ if n%2 == 0:
+    print(f' é par o numero: {n}')
+else: print(f'é impar o numero: {n}')
+#len
+lista = [2,4,6,8,10]
+print(len(lista))
+#append
+lista = [1,3,7,9]
+lista.append(5)
+print(lista)
+#del
+lista = [0,2,4,6,8]
+del lista[2]
+print(lista)
+#exercício do dia 3/04 pt.1
+lista = [1,2,3,4,5,6]
+soma = sum(lista)
+print(soma)
+#Tuplas
+tupla= (1,2,3,4,5,6,7)
+#tuplas porém com cores
+cores = ('vermelho', 'preto', 'branco', 'azul')
+print(cores)
+#tuplas com notas dos alunos
+notas = (6.5,7.0,8.5,3.0,5.9)
+#situação de decisão
+for nota in notas:
+    if nota >= 6.0:
+        print(f'nota:{nota} - aprovado')
+    else:
+        print(f'nota:{nota} - reprovado')
+#Dicionário
+alunos ={
+    'pedro':[7.5,8.0,6.0,9.5],
+    'maria':[6.0,5.5,7.0,8.0],
+    'joão':[8.5,9.0,7.5,6]
+}
+for alunos, notas in alunos.itemns():
+    print('aluno:', nome)
+    print('notas:', notas)
+    print()
+#set
+numeros=[1,2,3,4,4,5,5,6,6,7,8,9,10]
+numeros_sem_repeticao = set(numeros)
+print('valores sem repetição:', numeros_sem_repeticao)
+#def
+def soma (a,b):
+     return a + b
+print(soma(22,13))
+#mais complexo
+def varificar_par_impar(numero):
+    if numero % 2 ==0:
+        print('o número é par')
+    else:
+        print('o número é ímpar')
+    valor = int(input('digite um número:'))
+#lista, soma e média
+lista = [2,3,6,8,10]
+soma = sum(lista)
+média = soma/ len(lista)
+print (f'sua soma é:{soma}' f'sua média é: {média}')
+#fim do exercício
+#Área de um retângulo
+def area_retangulo(base, altura):
+    return base * altura
+base = float(input('digite a base do retângulo: '))
+altura = float(input('digite a altura do retângulo: '))
+print(f'a sua área é: {area_retangulo(base,altura)}')
+#Fim da Área do retângulo.
